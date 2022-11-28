41b831fd0243ef319a8087587a6229e88d596954
Eric Camellini <eric.camellini@gmail.com>
2022-11-28 12:04:08 +0100

    First recipe in JS

---
+++ recipe.js
@@ -0,0 +1,23 @@
+console.log("Welcome to the programming kitchen.");
+console.log("We're going to bake some cookies 🍪");
+
+let sugar = 80;
+let butter = 120;
+let flour = 200;
+let chocolate = 20;
+
+console.log("Here are our ingredients:");
+console.log("Sugar:", sugar);
+console.log("Butter:", butter);
+console.log("Flour:", flour);
+
+let mediumBowl;
+mediumBowl = butter + sugar;
+
+console.log("We mixed together butter & sugar:");
+console.log("Content of medium bowl:", mediumBowl);
+
+mediumBowl += flour + chocolate;
+
+console.log("We also added flour & chocolate...");
+console.log("Now the content is:", mediumBowl);
