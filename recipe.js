8dbd2070763d6eaba4be241821b51ae9971075e8
Eric Camellini <eric.camellini@gmail.com>
2022-11-28 12:24:38 +0100

    Emptying a variable using nulle

--- recipe.js
+++ recipe.js
@@ -1,3 +1,12 @@
+// This is a comment...
+// Comments have no effect on the code, they are just some
+// comments written by developers, for developers.
+// Which developers? Coworkers, or yourlself in the future, when you go back to this code.
+
+/* Everything after this i a comment, multiple lines...
+this is again the same comment...
+until we write */
+
 console.log("Welcome to the programming kitchen.");
 console.log("We're going to bake some cookies 🍪");
 
@@ -18,6 +27,12 @@ console.log("We mixed together butter & sugar:");
 console.log("Content of medium bowl:", mediumBowl);
 
 mediumBowl += flour + chocolate;
+// ^^ This is just another way to write the following instruction:
+// mediumBowl = mediumBowl + flour + chocolate
 
 console.log("We also added flour & chocolate...");
 console.log("Now the content is:", mediumBowl);
+
+console.log("Now we want to empty the medium bowl...");
+mediumBowl = null;
+console.log("Now the content of medium bowl is:", mediumBowl);
